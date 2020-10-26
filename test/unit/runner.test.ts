7ce9819740624cd0b529d1a5767e1e9d96ecc689
Mathias Schreck <mathias.schreck@joyn.de>
2020-10-26 16:54:27 +0100

    feat: calculate result summary

--- test/unit/runner.test.ts
+++ test/unit/runner.test.ts
@@ -22,7 +22,7 @@ function runnerFactory(overrides: Overrides = {}) {
 }
 
 test('executes all tests that have been added so far', () => {
-  const execute = sinon.fake();
+  const execute = sinon.fake.returns({});
   const runner = runnerFactory({ execute });
 
   runner.addTestCase({ title: 'foo', testFn: noop });
@@ -35,7 +35,7 @@ test('executes all tests that have been added so far', () => {
 });
 
 test('when calling runAll() a second time it executes all tests that have been added before and after the first run', () => {
-  const execute = sinon.fake();
+  const execute = sinon.fake.returns({});
   const runner = runnerFactory({ execute });
 
   runner.addTestCase({ title: 'foo', testFn: noop });
@@ -61,7 +61,13 @@ test('returns the aggregated result of all test cases', () => {
 
   assert.equal(result, {
     progress: 'completed',
-    totalCount: 2,
+    summary: {
+      totalCount: 2,
+      failedCount: 0,
+      successCount: 2,
+      completedCount: 2,
+      pendingCount: 0,
+    },
     testCaseResults: [
       {
         testCaseDetails: { title: 'foo' },
@@ -72,6 +78,28 @@ test('returns the aggregated result of all test cases', () => {
         result: { status: 'success', duration: 42 },
       },
     ],
+  });
+});
+
+test('calculates the correct amount of failed tests', () => {
+  const execute = sinon
+    .stub()
+    .onFirstCall()
+    .returns({ status: 'success', duration: 21 })
+    .onSecondCall()
+    .returns({ status: 'failure', duration: 42 });
+  const runner = runnerFactory({ execute });
+
+  runner.addTestCase({ title: 'foo', testFn: noop });
+  runner.addTestCase({ title: 'bar', testFn: noop });
+  const result = runner.runAll();
+
+  assert.equal(result.summary, {
+    totalCount: 2,
+    failedCount: 1,
+    successCount: 1,
+    completedCount: 2,
+    pendingCount: 0,
   });
 });
 
