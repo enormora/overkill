5eddc25f18df7b75dc1d0508f6ff1ff195afe872
Christian Rackerseder <git@echooff.de>
2022-10-27 10:16:11 +0200

    Use full file path when importing modules

--- source/runner.test.ts
+++ source/runner.test.ts
@@ -1,7 +1,7 @@
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import sinon, { type SinonSpy } from 'sinon';
-import { createRunner, type RunnerDependencies } from './runner';
+import { createRunner, type RunnerDependencies } from './runner.js';
 
 function noop() {}
 
